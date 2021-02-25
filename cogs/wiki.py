0bf9f7d44d58db446ca45793ec074c30c5ee218b
TigerTech96 <tparnell96@protonmail.com>
2021-02-25 12:18:12 -0600

    new function

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -2,6 +2,7 @@ import discord
 from discord import *
 from discord.ext import commands
 import wikipedia
+import wikia
 
 class Wiki(commands.Cog):
 
@@ -10,10 +11,15 @@ class Wiki(commands.Cog):
 		self.client = client
 		print ('Loaded Wikipedia search')
 
+	#@commands.command()
+	#async def wiki(self, ctx, *, question):
+		#str (question)
+		#await ctx.send (wikipedia.summary(question))
+
 	@commands.command()
-	async def wiki(self, ctx, *, question):
+	async def wiki(self, ctx, * , question):
 		str (question)
-		await ctx.send (wikipedia.summary(question))
+		await ctx.send (wikia.summary('Elite Dangerous' , question))
 
 def setup(client):
 	client.add_cog(Wiki(client))
