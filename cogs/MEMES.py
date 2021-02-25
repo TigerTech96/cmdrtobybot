ed5cfa6fdc0e7997222567bb80037e8904d8b981
TigerTech96 <tparnell96@protonmail.com>
2021-02-25 16:51:42 -0600

    fix

--- cogs/MEMES.py
+++ cogs/MEMES.py
@@ -51,6 +51,44 @@ class Memes(commands.Cog):
 				break #if it's playing it breaks
 			else:
 				await voice.disconnect() #if not it disconnects
+		@commands.command()
+	async def WHAT(self, ctx):
+		await ctx.message.delete()
+		song_there = os.path.isfile('WHAT.wav')
+		try:
+			if song_there:
+				os.remove('WHAT.wav')
+		except PermissionError:
+			await ctx.send('Error')
+			return
+		voiceChannel = ctx.message.author.voice.channel
+		await voiceChannel.connect()
+		voice = discord.utils.get(self.client.voice_clients , guild = ctx.guild)
+
+		ydl_opts = {
+			'format': 'bestaudio/best',
+			'postprocessors': [{
+				'key': 'FFmpegExtractAudio',
+				'preferredcodec': 'wav',
+				'preferredquality': '320',
+			}],
+		}
+		with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+			ydl.download(['https://www.youtube.com/watch?v=W1yM0wVnpFU'])
+		for file in os.listdir('./'):
+			if file.endswith('.wav'):
+				os.rename(file , 'WHAT.wav')
+		voice.play(discord.FFmpegPCMAudio('WHAT.wav'))
+		sleep (10)
+		voice = discord.utils.get(self.client.voice_clients , guild = ctx.guild)
+		while voice.is_playing(): #Checks if voice is playing
+			await asyncio.sleep(1) #While it's playing it sleeps for 1 second
+		else:
+			await asyncio.sleep(15) #If it's not playing it waits 15 seconds
+			while voice.is_playing(): #and checks once again if the bot is not playing
+				break #if it's playing it breaks
+			else:
+				await voice.disconnect() #if not it disconnects
 
 
 
