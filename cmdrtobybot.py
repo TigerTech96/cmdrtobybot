beee179b6b26591dcc021a448ad54e5b236e2ca6
TigerTech96 <tparnell96@protonmail.com>
2021-03-02 12:55:28 -0600

    Status Update

--- cmdrtobybot.py
+++ cmdrtobybot.py
@@ -25,7 +25,7 @@ logger.addHandler(handler)
 
 
 client = commands.Bot(command_prefix = '>' , intents = intents)
-status = cycle(['Elite Dangerous' , 'Jericho'])
+status = cycle(['The Mighty Ducks'])
 
 print('Loading extensions.....')
 for filename in os.listdir('./cogs'):
