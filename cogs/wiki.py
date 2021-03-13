f179a24bc84a1da7fa6a0fbf0e0a6c804c7f6112
TigerTech96 <tparnell96@protonmail.com>
2021-03-13 14:57:15 -0600

    another wiki fix attempt

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -13,19 +13,18 @@ class Wiki(commands.Cog):
 		self.client = client
 		print ('Loaded Wikipedia search')
 
-	@commands.command(brief = 'Returns some wikipedia info on a subject' , description = 'Retunrs info from wikipedia from the typed subject i.e >wiki Dogs')
+	@commands.command(brief = 'Returns some wikipedia info on a subject' , description = 'Returns info from wikipedia from the typed subject i.e >wiki Dogs')
 	#change comment
 	async def wiki(self, ctx , message=None):
 		str (message)
 		print (message)
 		try:
 			print ('trying to type summary')
-			await ctx.send (wikipedia.summary(suggestion1))
-			print (wikipedia.summary(suggestion1))
+			print (wikipedia.summary(message))
 		except wikipedia.exceptions.DisambiguationError as e:
 			print ('Attempting disambiguation')
 			wikioptions = e.options
-			wikiotion = str (wikioptions)
+			wikioption = str (wikioptions)
 			await ctx.send ('Did you mean:' + wikioptions)
 		
 		
